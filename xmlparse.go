74312908929594f4f8de51ab7779b8acd65d3e78
pbberlin <peter.buchmann@web.de>
2016-06-22 00:07:33 +0200

    parsing deeply nested xml without xmplpath

--- xmlparse.go
+++ xmlparse.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
@@ -15,57 +14,40 @@ import (
 	"github.com/zew/awis/util"
 )
 
-type TGlobal struct {
-	Rank int `xml:aws:Rank`
+type Site struct {
+	DataUrl                    string
+	GlobalRank                 int     `xml:"Global>Rank"`
+	CountryRank                int     `xml:"Country>Rank"`
+	CountryReachPerMillion     int     `xml:"Country>Reach>PerMillion"`
+	CountryPageViewsPerMillion int     `xml:"Country>PageViews>PerMillion"`
+	CountryPageViewsPerUser    float64 `xml:"Country>PageViews>PerUser"`
 }
-type TPageViews struct {
-	PerMillion int     `xml:PerMillion`
-	PerUser    float64 `xml:PerUser`
-}
-type TReach struct {
-	PerMillion int `xml:PerMillion`
-}
-
-type TCountry struct {
-	Rank      int        `xml:Rank`
-	Reach     TReach     `xml:Reach`
-	PageViews TPageViews `xml:PageViews`
-}
-
-type TSite struct {
-	DataUrl string   `xml:DataUrl`
-	Country TCountry `xml:Country`
-	Global  TGlobal  `xml:Global`
-}
-
-type TSites struct {
-	Sites []TSite `xml:Sites`
+type Result struct {
+	// Sites []Site `xml:"TopSitesResponse>Response>TopSitesResult>Alexa>TopSites>Country>Sites>Site"`
+	Sites []Site `xml:"Response>TopSitesResult>Alexa>TopSites>Country>Sites>Site"` // omit the outmost tag name TopSitesResponse
 }
 
 func xmlparse(c *iris.Context) {
-	dat, err := ioutil.ReadFile("example.xml")
-	util.CheckErr(err)
 
+	dat, err := ioutil.ReadFile("example1.xml")
+	util.CheckErr(err)
 	// c.Text(200, string(dat))
 
-	rdr := bytes.NewReader(dat)
-	sites := TSites{}
-
-	err = xml.NewDecoder(rdr).Decode(&sites)
-
+	sites := Result{}
+	err = xml.Unmarshal(dat, &sites)
 	if err != nil {
 		c.Text(200, err.Error())
-	} else {
-		c.Text(200, "suck sess - in decoding<br>\n")
 	}
 	res := util.IndentedDump(sites)
 	c.Text(200, res)
-	c.Text(200, "<br>\n<br>\n")
 
-	path := xmlpath.MustCompile("/library/book/isbn")
-	path = xmlpath.MustCompile("//Alexa")
-	path = xmlpath.MustCompile("/TopSitesResponse/Response/TopSitesResult/Alexa/TopSites")
-	path = xmlpath.MustCompile("/TopSitesResponse/Response/TopSitesResult/Alexa/TopSites/Country")
+}
+
+//
+//
+func xmlparseExamplePath(c *iris.Context) {
+
+	path := xmlpath.MustCompile("//Alexa")
 	path = xmlpath.MustCompile("/TopSitesResponse/Response/TopSitesResult/Alexa/TopSites/Country/Sites/Site")
 
 	file, err := os.Open("example2.xml")
@@ -74,36 +56,66 @@ func xmlparse(c *iris.Context) {
 	if err != nil {
 		c.Text(200, err.Error())
 	}
-	_ = root
-	_ = path
 
 	if path.Exists(root) {
-		c.Text(200, "path exists<br>\n")
+		c.Text(200, "path exists\n")
 
 		if false {
 			if value, ok := path.String(root); ok {
-				c.Text(200, value+"<br>\n")
+				c.Text(200, value+"\n")
 			}
 			if subBytes, ok := path.Bytes(root); ok {
 				c.RequestCtx.Write(subBytes)
 			}
 		}
 		nodes := path.Iter(root)
-		c.Text(200, "Nodes are there<br>\n")
+		c.Text(200, "Nodes are there\n")
 		for nodes.Next() {
 			node := nodes.Node()
-			str1 := fmt.Sprintf("Node is %v  <br>\n", node)
-			c.Text(200, str1+"<br>\n")
-
-			rdr := bytes.NewReader(node.Bytes())
-			site := TSite{}
-			err = xml.NewDecoder(rdr).Decode(&site)
-			res := util.IndentedDump(site)
-			c.Text(200, res)
-
+			str1 := fmt.Sprintf("\n\nNode is %+v  \n\n", node)
+			c.Text(200, str1)
 		}
 
 	} else {
-		c.Text(200, "path NOT there<br>\n")
+		c.Text(200, "path NOT there\n")
 	}
+
+}
+
+//
+//
+func xmlparseExampleSimple(c *iris.Context) {
+	type Email struct {
+		Addr string
+	}
+	type Result1 struct {
+		FullName string
+		Email    []Email
+		Groups   []string `xml:"Group>Value"`
+	}
+	v := Result1{}
+	data := `
+   			<Person>
+   				<FullName>Grace R. Emlin</FullName>
+   				<Email>
+   					<Addr>gre@example.com</Addr>
+   				</Email>
+   				<Email>
+   					<Addr>gre@work.com</Addr>
+   				</Email>
+   				<Group>
+   					<Value>Friends</Value>
+   					<Value>Squash</Value>
+   				</Group>
+   				<City>Hanga Roa</City>
+   				<State>Easter Island</State>
+   			</Person>
+   		`
+	err := xml.Unmarshal([]byte(data), &v)
+	if err != nil {
+		c.Text(200, err.Error())
+	}
+	vs := util.IndentedDump(v)
+	c.Text(200, vs)
+
 }
